cf8a84f3b929e258ce0b9ae63bab805639eb4f2c
Federico Santana <fede.santana42@gmail.com>
2021-03-26 23:47:54 -0300

    remove innecesary, add consoles.log

--- wp-content/themes/accelerate/podcasting-platform/index.js
+++ wp-content/themes/accelerate/podcasting-platform/index.js
@@ -1,25 +1,23 @@
-import {xmlParse, htmlParse} from './components/parser.js'
+import { xmlParse, htmlParse } from './components/parser.js'
 import Podcasts, { ORDER_STATE } from './components/podcasts.js'
 import Podcast, { PODCAST_STATE } from './components/podcast.js'
 import Player from './components/player.js'
 import PodcastingPlatform from './components/podcastingPlatform.js'
 
 const RSS_URL = `https://anchor.fm/s/2fe1f008/podcast/rss`;
-const podcasts = [];
 
 // Wait until your DOM is fully loaded
 document.addEventListener("DOMContentLoaded", async function () {
     const podcastingPlatform = new PodcastingPlatform(RSS_URL);
-  
+
     const podcasts = await podcastingPlatform.getItems();
 
     const podcastHTML = document.getElementById(`podcastsContainer`);
-    
-    const renderOutput = document.createElement("podcasting-platform-podcasts");
 
-    renderOutput.innerHTML = podcastingPlatform.podcasts.render();
-    
-    podcastHTML.appendChild(renderOutput);
+    console.log(podcastingPlatform.podcasts.render()[0]);
+    console.log(typeof (podcastingPlatform.podcasts.render()[0]));
+
+    podcastHTML.append(podcastingPlatform.podcasts.render());
     podcastHTML.appendChild(podcastingPlatform.player.render());
 
 
